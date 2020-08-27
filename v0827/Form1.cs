5102e9a083954b0c004b4e2a47260000b9d288da
鎌田　拓臣 <yuuteayuu335dat@gmail.com>
2020-08-27 09:42:11 +0900

    Initial commit

---
+++ v0827/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0827
+{
+    public partial class Form1 : Form
+    {
+        int vx = -10;
+        int vy = -10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point mp = MousePosition;
+            label2.Left = mp.X;
+            label2.Top = mp.Y;
+            label2.Text = "" + mp.X + "," + mp.Y;
+
+            label1.Left += vx;
+            label1.Top += vy;
+            }
+        }
+    }
+
